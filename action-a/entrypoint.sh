bb3f4d7aebf04ef544cba7c0bc2c73a89bdc53b2
James Heanly <james.heanly@gmail.com>
2020-08-14 16:51:50 +1000

    first action

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -3,3 +3,4 @@
 sh -c "echo Hello world my name is $INPUT_MY_NAME"
 sh -c "echo Hello world my ref is $GITHUB_REF"
 sh -c "echo Hello world my BRANCH NAME is $INPUT_BRANCH_NAME"
+sh -c "echo Hello world my TOKEN is $NPM_TOKEN_AHM"
